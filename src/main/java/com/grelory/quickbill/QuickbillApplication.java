a08d9db9ccfe661570b0d32e25b9c26aac4404d5
grelory <gmpwm99@gmail.com>
2024-12-07 14:21:38 +0100

    ZQB-1 Create initial Spring project

---
+++ src/main/java/com/grelory/quickbill/QuickbillApplication.java
@@ -0,0 +1,13 @@
+package com.grelory.quickbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickbillApplication.class, args);
+	}
+
+}
